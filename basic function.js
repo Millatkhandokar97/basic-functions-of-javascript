d7587d8f21d0a7451576291e14aaa3d71278965d
Millat Khandakar <109697680+Millatkhandokar97@users.noreply.github.com>
2024-10-15 23:39:36 +0600

    basic function in javascript

---
+++ basic function.js
@@ -0,0 +1,16 @@
+//basic function 
+
+function add (){
+    let a = 10
+    let b = 20
+    return a + b
+}
+console.log(add(10, 20));
+
+
+
+function add (a, b){
+    let result = a + b
+    return result
+}
+console.log(add(20, 30));
